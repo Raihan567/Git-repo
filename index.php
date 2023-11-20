851e95fea45bcd798f19594638a439bcc16c292e
Raihan567 <aburaihansarker567@gmail.com>
2023-11-20 10:46:45 +0600

    learn back to previous version

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$name = 'Abu Raihan';
+$a = '34687';
+$b = 34687;
+$c = 34687.234;
+$d = true;
+$e = false;
+$f = ['a', 'b', 'c'];
+// $g = {};
+
+var_dump($name);
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
+var_dump($f);
+// var_dump($g);
